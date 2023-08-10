d3ccde438d4dfc20ec2f79f649bffa6c9a149351
pwp114514 <117086223+pwp114514@users.noreply.github.com>
2023-08-10 11:42:26 +0800

    Update AtlasFrameMaker.hx

--- source/animateatlas/AtlasFrameMaker.hx
+++ source/animateatlas/AtlasFrameMaker.hx
@@ -14,7 +14,7 @@ import flixel.graphics.FlxGraphic;
 import flixel.graphics.frames.FlxFramesCollection;
 import flixel.graphics.frames.FlxFrame;
 
-#if （desktop || android)
+#if android
 import sys.FileSystem;
 import sys.io.File;
 #else
