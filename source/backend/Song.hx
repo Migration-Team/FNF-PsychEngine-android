a3b6aff2e17e35500d7009138bb23c9a80110fbf
pwp114514 <117086223+pwp114514@users.noreply.github.com>
2023-08-12 16:39:49 +0800

    Update Song.hx

--- source/backend/Song.hx
+++ source/backend/Song.hx
@@ -102,7 +102,7 @@ class Song
 		var formattedFolder:String = Paths.formatToSongPath(folder);
 		var formattedSong:String = Paths.formatToSongPath(jsonInput);
 		#if MODS_ALLOWED
-		var moddyFile:String = Paths.modsJson(formattedFolder + '/' + formattedSong);
+		var moddyFile:String = SUtil.getPath() + Paths.modsJson(formattedFolder + '/' + formattedSong);
 		if(FileSystem.exists(moddyFile)) {
 			rawJson = File.getContent(moddyFile).trim();
 		}
@@ -110,9 +110,9 @@ class Song
 
 		if(rawJson == null) {
 			#if sys
-			rawJson = File.getContent(Paths.json(formattedFolder + '/' + formattedSong)).trim();
+			rawJson = File.getContent(SUtil.getPath() + Paths.json(formattedFolder + '/' + formattedSong)).trim();
 			#else
-			rawJson = Assets.getText(Paths.json(formattedFolder + '/' + formattedSong)).trim();
+			rawJson = Assets.getText(SUtil.getPath() + Paths.json(formattedFolder + '/' + formattedSong)).trim();
 			#end
 		}
 
